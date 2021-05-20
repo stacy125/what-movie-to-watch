543061a231315a668b15529610e6100475a64443
stacy leitstein <unique523@yahoo.com>
2021-05-19 20:22:33 -0400

    fixed imdb

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ThumbsUp from '../assets/thumb-up.png'
 import ThumbsDown from '../assets/thumb-down.png'
 
-const Thumb = ({imbdRating}) => {
+const Thumb = ({imdbRating}) => {
     const thumbsUpIcon = <img className='thumb-icon' src={ThumbsUp} alt='thumbs up icon'/>
 
     const thumbsDownIcon = <img className='thumb-icon' src={ThumbsDown} alt='thumbs down icon'/>
 
-    const ratingCheck = imbdRating > 5 ? thumbsUpIcon : thumbsDownIcon
+    const ratingCheck = imdbRating > 5 ? thumbsUpIcon : thumbsDownIcon
     return (
         <div>
             {ratingCheck}
